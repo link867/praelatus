268519c482afc5bcbe2470e3006cacdaaf8a4ca4
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-10 18:26:00 -0500

    fix bad log call

--- praelatus.go
+++ praelatus.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"log"
+
 	"github.com/praelatus/backend/api"
 )
 
 func main() {
-	fmt.Println("Starting Praelatus!")
-	fmt.Println("Initializing database...")
+	log.Println("Starting Praelatus!")
+	log.Println("Initializing database...")
 
 	// Will be used for logging
 	// mw := io.MultiWriter(os.Stdout)
 
-	log.Info("Ready to serve requests!")
+	log.Println("Ready to serve requests!")
 	port := os.Getenv("PRAELATUS_PORT")
 	if port == "" {
 		port = ":8080"
