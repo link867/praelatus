1fedf5d657049488b1285d930d7abbfbe3aa07c5
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-12 17:54:24 -0500

    Added label routes for GetAllLables, CreateLabel, GetLabel, DeleteLabel, UpdateLabel.

--- api/labels.go
+++ api/labels.go
@@ -3,14 +3,19 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/praelatus/backend/models"
 )
 
 func InitLabelRoutes() {
-	BaseRoutes.Tickets.Handle("/", mw.Default(GetAllLabels)).Methods("GET")
-	BaseRoutes.Tickets.Handle("/", mw.Default(CreateLabel)).Methods("POST")
-	BaseRoutes.Tickets.Handle("/{id}", mw.Default(DeleteLabel)).Methods("DELETE")
-	BaseRoutes.Tickets.Handle("/{id}", mw.Default(UpdateLabel)).Methods("PUT")
-	BaseRoutes.Tickets.Handle("/search", mw.Default(SearchLabels)).Methods("GET")
+	BaseRoutes.Labels.Handle("/", mw.Default(GetAllLabels)).Methods("GET")
+	BaseRoutes.Labels.Handle("/", mw.Default(CreateLabel)).Methods("POST")
+	BaseRoutes.Labels.Handle("/{idOrName}", mw.Default(GetLabel)).Methods("GET")
+	BaseRoutes.Labels.Handle("/{idOrName}", mw.Default(DeleteLabel)).Methods("DELETE")
+	BaseRoutes.Labels.Handle("/{idOrName}", mw.Default(UpdateLabel)).Methods("PUT")
+	BaseRoutes.Labels.Handle("/search", mw.Default(SearchLabels)).Methods("GET")
 }
 
 // GetAllLabels will return a JSON array of all labels from the store.
@@ -30,4 +35,105 @@ func GetAllLabels(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(jsn)
+}
+
+// GetLabel will return a JSON representation of a model.
+func GetLabel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	lbl := &models.Label{}
+
+	i, err := strconv.Atoi(vars["idOrName"])
+	if err != nil {
+		lbl.Name = vars["idOrName"]
+	}
+
+	label, err := Store.Labels().Get(lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	jsn, err := json.Marshal(label)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write(jsn)
+}
+
+// CreateLabel creates a label in the db and return a JSON object of
+func CreateLabel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	lbl := &models.Label{}
+
+	i, err := strconv.Atoi(vars["idOrName"])
+	if err != nil {
+		lbl.Name = vars["idOrName"]
+	}
+
+	lbl.ID = i
+
+	err := Store.Labels().New(lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write(jsn)
+}
+
+// UpdateLabel updates the label in the db and returns a message indicating
+// success or failure.
+func UpdateLabel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	lbl := &models.Label{}
+
+	i, err := strconv.Atoi(vars["idOrName"])
+	if err != nil {
+		lbl.Name = vars["idOrName"]
+	}
+
+	err := Store.Label().Save(lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte("Label successfully updated"))
+
+}
+
+// DeleteLabel deletes labels from the db and returns a repsonse indicating
+// success of failure.
+func DeleteLabel(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	lbl := &models.Label{}
+
+	i, err := strconv.Atoi(vars["idOrName"])
+	if err != nil {
+		lbl.Name = vars["idOrName"]
+	}
+
+	label, err := Store.Labels().Remove(lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte("Label successfully deleted"))
+}
+
+// SearchLabels
+func SearchLabels(w http.ResponseWriter, r *http.Request) {
+
 }
