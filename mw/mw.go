93d2fff9bdd7509efd7ce323eb882acc0835dcff
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-16 12:13:12 -0500

    implemened a new default function and added the Middleware type

---
+++ mw/mw.go
@@ -0,0 +1,20 @@
+package mw
+
+import "net/http"
+
+// Middleware is a function which will modify or add to a http.Request in some
+// way
+type Middleware func(next http.Handler) http.Handler
+
+var defaultMW = []Middleware{Logger, Auth}
+
+// Default will add the default middleware stack to the given http.Handler and
+// return a handler with the full stack
+func Default(next http.Handler) http.Handler {
+	var h = next
+	for _, m := range defaultMW {
+		h = m(h)
+	}
+
+	return h
+}
