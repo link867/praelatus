70ee937bc0fdd92e8cce9b557f542e8c385e6dba
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-18 17:47:10 -0500

    Added more tests to api_tests.go

--- api/api_tests.go
+++ api/api_tests.go
@@ -146,44 +146,328 @@ func (ms *MockTeamStore) Get(t *models.Team) error {
 	return nil
 }
 
-// func(ms *MockTeamStore) GetAll() ([]models.Team, error) {
-// 	return []models.Team, error
-// }
+func(ms *MockTeamStore) GetAll() ([]models.Team, error) {
+ 	return []models.Team{
+		models.Team{
+			Team.ID = 1
+			Team.Name = "A"
+			Team.Lead = models.User{
+				1,
+				"foouser",
+				"foopass",
+				"foo@foo.com",
+				"Foo McFooserson",
+				"",
+					"",
+				false,
+				true,
+				models.Settings{},
+			},
+			Team.Members = []models.User{
+				models.User{
+					1,
+					"foouser",
+					"foopass",
+					"foo@foo.com",
+					"Foo McFooserson",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+				models.User{
+						2,
+					"foouser",
+						"foopass",
+					"foo@foo.com",
+					"Foo McFooserson",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+			},			
+		},
+		models.Team{
+			Team.ID = 1
+			Team.Name = "A"
+			Team.Lead = models.User{
+				2,
+				"foouser3",
+				"foopass",
+				"foo@foo3.com",
+				"Foo McFooserson3",
+				"",
+				"",
+				false,
+				true,
+				models.Settings{},
+			}
+			Team.Members = []models.User{
+				models.User{
+					3,
+					"foouser3",
+					"foopass",
+					"foo@foo3.com",
+					"Foo McFooserson3",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+				models.User{
+					4,
+					"foouser4",
+					"foopass",
+					"foo@foo4.com",
+					"Foo McFooserson4",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},		
+			},
+		},
+	},
+	nil
+}
 
-// func(ms *MockTeamStore) GetForUser() ([]models.Team, error) {
-// 	return []models.Team, error
-// }
+func(ms *MockTeamStore) GetForUser(m *models.User) ([]models.Team, error) {
+	// If I am getting the team that the user is on,
+	// why am I returning a slice? 
+	return []models.Team{
+		models.Team{
+			Team.ID = 1
+			Team.Name = "A"
+			Team.Lead = models.User{
+				1,
+				"foouser",
+				"foopass",
+				"foo@foo.com",
+				"Foo McFooserson",
+				"",
+					"",
+				false,
+				true,
+				models.Settings{},
+			},
+			Team.Members = []models.User{
+				models.User{
+					1,
+					"foouser",
+					"foopass",
+					"foo@foo.com",
+					"Foo McFooserson",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+				models.User{
+						2,
+					"foouser",
+						"foopass",
+					"foo@foo.com",
+					"Foo McFooserson",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+			},			
+		},
+		models.Team{
+			Team.ID = 1
+			Team.Name = "A"
+			Team.Lead = models.User{
+				2,
+				"foouser3",
+				"foopass",
+				"foo@foo3.com",
+				"Foo McFooserson3",
+				"",
+				"",
+				false,
+				true,
+				models.Settings{},
+			}
+			Team.Members = []models.User{
+				models.User{
+					3,
+					"foouser3",
+					"foopass",
+					"foo@foo3.com",
+					"Foo McFooserson3",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},
+				models.User{
+					4,
+					"foouser4",
+					"foopass",
+					"foo@foo4.com",
+					"Foo McFooserson4",
+					"",
+					"",
+					false,
+					true,
+					models.Settings{},
+				},		
+			},
+		},
+	},
+	nil
 
-// func(ms *MockTeamStore) AddMembers() error {
-// 	return error
-// }
+}
 
-// func(ms *MockTeamStore) New() error {
-// 	return error
-// }
+func(ms *MockTeamStore) AddMembers(m *models.Team models.User) error {
+	return nil
+}
 
-// func(ms *MockTeamStore) Save() error {
-// 	return error
-// }
+func(ms *MockTeamStore) New(*models.Team) error {
+	return nil
+}
 
-// func(ms *MockTeamStore) Remove() error {
-// 	return error
-// }
+func(ms *MockTeamStore) Save() error {
+	return nil
+}
 
-// //A Mock LabelStore struct
-// type MockLabelStore struct {}
+func(ms *MockTeamStore) Remove() error {
+	return nil
+}
 
-// func (ms *MockLabelStore) Get() error {
-// 	return nil
-// }
+//A Mock LabelStore struct
+type MockLabelStore struct {}
 
-// func (ms)
+func (ms *MockLabelStore) Get(l *models.Label) error {
+	return nil
+}
 
-// //A Mock FieldStore struct
-// type MockFieldStore struct {}
+
+//A Mock FieldStore struct
+type MockFieldStore struct {}
+
+func (MockFieldStore) Get(f *models.Field) error {
+	f.ID = 1
+	f.Name = "FooField"
+	f.DataType = "FooDataType"
+	f.Options = "" // Correct?
+	return nil
+}
+
+func (MockFieldStore) GetAll() ([]models.Field, error) {
+	return []models.Field{
+		models.Field{
+			Field.ID = 1
+			Field.Name = "FooField"
+			Field.DataType = "FooDataType"
+			Field.Options = "" // Correct?
+		},
+		models.Field{
+			Field.ID = 2
+			Field.Name = "FooField2"
+			Field.DataType = "FooDataType2"
+			Field.Options = "" //Correct?
+		}
+	}, nil
+}
+
+func (MockFieldStore) GetByProject(p models.Project) {
+	return []models.Field{
+		models.Field{
+			Field.ID = 1
+			Field.Name = "FooField"
+			Field.DataType = "FooDataType"
+			Field.Options = "" // Correct?
+		},
+		models.Field{
+			Field.ID = 2
+			Field.Name = "FooField2"
+			Field.DataType = "FooDataType2"
+			Field.Options = "" //Correct?
+		}
+	}, nil
+}
+
+func (MockFieldStore) AddToProject(p models.Project, f *models.Field, t ...models.TicketType) error {
+	return nil
+}
+
+func (MockFieldStore) New(f *models.Field) error {
+	return nil
+}
+
+func (MockFieldStore) Save(f *models.Field) error {
+	return nil
+}
+
+func (MockFieldStore) Remove(f *models.Field) error {
+	return nil
+}
 
 // //A Mock TicketStore struct
-// type MockTicketSotre struct {}
+type MockTicketStore struct {}
+
+func (MockTicketStore) Get(t *models.Ticket) error {
+	t.ID = 1
+	t.CreatedDate = "000000"
+	t.UpdatedDate = "000000"
+	t.Key = "fooKey"
+	t.Summary = "fooSummary"
+	t.Description = "fooDescription"
+	t.Fields = []models.FieldValue{
+		FieldValue{
+
+		},
+		FieldValue{
+
+		},
+	}
+	t.Labels = []models.Label{
+		models.Label{
+
+		},
+		models.Label{
+
+		}
+	}
+	t.Type = ""
+	t.Reporter = models.User{
+			4,
+			"foouser4",
+			"foopass",
+			"foo@foo4.com",
+			"Foo McFooserson4",
+			"",
+			"",
+			false,
+			true,
+			models.Settings{},
+	}
+	t.Reporter = models.User{
+			4,
+			"foouser4",
+			"foopass",
+			"foo@foo4.com",
+			"Foo McFooserson4",
+			"",
+			"",
+			false,
+			true,
+			models.Settings{},
+	}
+	t.Status = ""
+	return nil
+}
 
 // //A Mock TypeStore struct
 // type MockTypeStore struct {}
