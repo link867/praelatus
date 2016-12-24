188e8ff2e3d4fb0a8c93657b4ecfe5ef3376d3f9
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-24 13:27:17 -0500

    write test for create project

--- api/projects_test.go
+++ api/projects_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
@@ -47,6 +48,29 @@ func TestGetAllProjects(t *testing.T) {
 
 	if len(p) != 2 {
 		t.Errorf("Expected 2 Got %d\n", len(p))
+	}
+
+	t.Log(w.Body)
+}
+
+func TestCreateProject(t *testing.T) {
+	p := models.Project{Name: "Grumpy Cat", Key: "NOPE"}
+	byt, _ := json.Marshal(p)
+	rd := bytes.NewReader(byt)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/projects", rd)
+	testAdminLogin(r)
+
+	Router.ServeHTTP(w, r)
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s", e.Error())
+	}
+
+	if p.ID != 1 {
+		t.Errorf("Expected 1 Got %d", p.ID)
 	}
 
 	t.Log(w.Body)
