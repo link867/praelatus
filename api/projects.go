03e76191c92aef01c604b497f55cb41410caaced
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-27 12:59:41 -0500

    finished implementing CRUD for projects

--- api/projects.go
+++ api/projects.go
@@ -11,12 +11,15 @@ import (
 )
 
 func initProjectRoutes() {
-	Router.Handle("/projects", mw.Default(getAllProjects)).Methods("GET")
-	Router.Handle("/projects", mw.Default(createProject)).Methods("POST")
-	Router.Handle("/projects/{pkey}", mw.Default(getProject)).Methods("GET")
+	Router.Handle("/projects", mw.Default(GetAllProjects)).Methods("GET")
+	Router.Handle("/projects", mw.Default(CreateProject)).Methods("POST")
+	Router.Handle("/projects/{pkey}", mw.Default(GetProject)).Methods("GET")
+	Router.Handle("/projects/{pkey}", mw.Default(RemoveProject)).Methods("DELETE")
+	Router.Handle("/projects/{pkey}", mw.Default(UpdateProject)).Methods("PUT")
 }
 
-func getProject(w http.ResponseWriter, r *http.Request) {
+// GetProject will get a project by it's project key
+func GetProject(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	p := models.Project{
@@ -31,13 +34,13 @@ func getProject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sendJson(w, p)
+	sendJSON(w, p)
 }
 
-// getAllProjects will get all the projects on this instance that the user has
+// GetAllProjects will get all the projects on this instance that the user has
 // permissions to
 // TODO handler permissions
-func getAllProjects(w http.ResponseWriter, r *http.Request) {
+func GetAllProjects(w http.ResponseWriter, r *http.Request) {
 	projects, err := Store.Projects().GetAll()
 	if err != nil {
 		w.WriteHeader(500)
@@ -46,10 +49,12 @@ func getAllProjects(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sendJson(w, projects)
+	sendJSON(w, projects)
 }
 
-func createProject(w http.ResponseWriter, r *http.Request) {
+// CreateProject will create a project based on the JSON representation sent to
+// the API
+func CreateProject(w http.ResponseWriter, r *http.Request) {
 	var p models.Project
 
 	u := mw.GetUser(r.Context())
@@ -76,5 +81,61 @@ func createProject(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	sendJson(w, p)
+	sendJSON(w, p)
+}
+
+// RemoveProject will remove the project indicated by the key passed in as a
+// url parameter
+func RemoveProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	err := Store.Projects().Remove(models.Project{Key: vars["key"]})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	w.Write([]byte{})
+
+}
+
+// UpdateProject will update a project based on the JSON representation sent to
+// the API
+func UpdateProject(w http.ResponseWriter, r *http.Request) {
+	var p models.Project
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&p)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid body"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Projects().New(&p)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, p)
 }
