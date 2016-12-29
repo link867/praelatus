d2e0c51be3407c343ce5183c159ac5a68d040c43
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-29 11:05:57 -0500

    wrote team routes and tests

---
+++ api/teams.go
@@ -0,0 +1,161 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/praelatus/backend/models"
+	"github.com/praelatus/backend/mw"
+)
+
+func initTeamRoutes() {
+	Router.Handle("/teams", mw.Default(GetAllTeams)).Methods("GET")
+	Router.Handle("/teams", mw.Default(CreateTeam)).Methods("POST")
+
+	Router.Handle("/teams/{id}", mw.Default(GetTeam)).Methods("GET")
+	Router.Handle("/teams/{id}", mw.Default(UpdateTeam)).Methods("PUT")
+	Router.Handle("/teams/{id}", mw.Default(RemoveTeam)).Methods("DELETE")
+}
+
+// GetAllTeams will retrieve all teams from the DB and send a JSON response
+func GetAllTeams(w http.ResponseWriter, r *http.Request) {
+	u := mw.GetUser(r.Context())
+	if u == nil {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in to view all teams"))
+		return
+	}
+
+	teams, err := Store.Teams().GetAll()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, teams)
+}
+
+// CreateTeam will create a team in the database based on the JSON sent by the
+// client
+func CreateTeam(w http.ResponseWriter, r *http.Request) {
+	var t models.Team
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("malformed json"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Teams().New(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// GetTeam will return the json representation of a team in the database
+func GetTeam(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/teams/"):]
+
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
+	}
+
+	t := models.Team{ID: int64(i)}
+
+	err = Store.Teams().Get(&t)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// UpdateTeam will update a project based on the JSON representation sent to
+// the API
+func UpdateTeam(w http.ResponseWriter, r *http.Request) {
+	var t models.Team
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid body"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Teams().New(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// RemoveTeam will remove the project indicated by the id passed in as a
+// url parameter
+func RemoveTeam(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	i, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Teams().Remove(models.Team{ID: int64(i)})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	w.Write([]byte{})
+}
---
+++ api/teams_test.go
@@ -0,0 +1,78 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/praelatus/backend/models"
+)
+
+func TestGetTeam(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/teams/1", nil)
+
+	Router.ServeHTTP(w, r)
+
+	var p models.Team
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s\n", e.Error())
+	}
+
+	if p.ID != 1 {
+		t.Errorf("Expected 1 Got %d\n", p.ID)
+	}
+
+	t.Log(w.Body)
+}
+
+func TestGetAllTeams(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/teams", nil)
+	testLogin(r)
+
+	Router.ServeHTTP(w, r)
+
+	var p []models.Team
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s\n", e.Error())
+	}
+
+	if p[0].Name != "A" {
+		t.Errorf("Expected A Got %s\n", p[0].Name)
+	}
+
+	if len(p) != 2 {
+		t.Errorf("Expected 2 Got %d\n", len(p))
+	}
+
+	t.Log(w.Body)
+}
+
+func TestCreateTeam(t *testing.T) {
+	p := models.Team{Name: "Snug"}
+	byt, _ := json.Marshal(p)
+	rd := bytes.NewReader(byt)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/teams", rd)
+	testAdminLogin(r)
+
+	Router.ServeHTTP(w, r)
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s", e.Error())
+	}
+
+	if p.ID != 1 {
+		t.Errorf("Expected 1 Got %d", p.ID)
+	}
+
+	t.Log(w.Body)
+}
