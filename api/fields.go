29ac1748eabd54a0dbfb56cd18974a1f9823f10d
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-29 11:05:35 -0500

    wrote field routes and tests

---
+++ api/fields.go
@@ -0,0 +1,161 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/praelatus/backend/models"
+	"github.com/praelatus/backend/mw"
+)
+
+func initFieldRoutes() {
+	Router.Handle("/fields", mw.Default(GetAllFields)).Methods("GET")
+	Router.Handle("/fields", mw.Default(CreateField)).Methods("POST")
+
+	Router.Handle("/fields/{id}", mw.Default(GetField)).Methods("GET")
+	Router.Handle("/fields/{id}", mw.Default(UpdateField)).Methods("PUT")
+	Router.Handle("/fields/{id}", mw.Default(DeleteField)).Methods("DELETE")
+}
+
+// GetAllFields will retrieve all fields from the DB and send a JSON response
+func GetAllFields(w http.ResponseWriter, r *http.Request) {
+	u := mw.GetUser(r.Context())
+	if u == nil {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in to view all fields"))
+		return
+	}
+
+	fields, err := Store.Fields().GetAll()
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, fields)
+}
+
+// CreateField will create a field in the database based on the JSON sent by the
+// client
+func CreateField(w http.ResponseWriter, r *http.Request) {
+	var t models.Field
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("malformed json"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Fields().New(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// GetField will return the json representation of a field in the database
+func GetField(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/fields/"):]
+
+	i, err := strconv.Atoi(id)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
+	}
+
+	t := models.Field{ID: int64(i)}
+
+	err = Store.Fields().Get(&t)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// UpdateField will update a project based on the JSON representation sent to
+// the API
+func UpdateField(w http.ResponseWriter, r *http.Request) {
+	var t models.Field
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid body"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Fields().New(&t)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, t)
+}
+
+// DeleteField will remove the project indicated by the id passed in as a
+// url parameter
+func DeleteField(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	u := mw.GetUser(r.Context())
+	if u == nil || !u.IsAdmin {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in as a system administrator to create a project"))
+		return
+	}
+
+	i, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Fields().Remove(models.Field{ID: int64(i)})
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	w.Write([]byte{})
+}
---
+++ api/fields_test.go
@@ -0,0 +1,78 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/praelatus/backend/models"
+)
+
+func TestGetField(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/fields/1", nil)
+
+	Router.ServeHTTP(w, r)
+
+	var p models.Field
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s\n", e.Error())
+	}
+
+	if p.ID != 1 {
+		t.Errorf("Expected 1 Got %d\n", p.ID)
+	}
+
+	t.Log(w.Body)
+}
+
+func TestGetAllFields(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/fields", nil)
+	testLogin(r)
+
+	Router.ServeHTTP(w, r)
+
+	var p []models.Field
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s\n", e.Error())
+	}
+
+	if p[0].Name != "String Field" {
+		t.Errorf("Expected String Field Got %s\n", p[0].Name)
+	}
+
+	if len(p) != 2 {
+		t.Errorf("Expected 2 Got %d\n", len(p))
+	}
+
+	t.Log(w.Body)
+}
+
+func TestCreateField(t *testing.T) {
+	p := models.Field{Name: "Snug"}
+	byt, _ := json.Marshal(p)
+	rd := bytes.NewReader(byt)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/fields", rd)
+	testAdminLogin(r)
+
+	Router.ServeHTTP(w, r)
+
+	e := json.Unmarshal(w.Body.Bytes(), &p)
+	if e != nil {
+		t.Errorf("Failed with error %s", e.Error())
+	}
+
+	if p.ID != 1 {
+		t.Errorf("Expected 1 Got %d", p.ID)
+	}
+
+	t.Log(w.Body)
+}
