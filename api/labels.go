5427239ec81baf35670210e1cabce2806042d34e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-12-29 11:02:15 -0500

    fix routes and handlers

--- api/labels.go
+++ api/labels.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -13,10 +14,10 @@ import (
 func initLabelRoutes() {
 	Router.Handle("/labels", mw.Default(GetAllLabels)).Methods("GET")
 	Router.Handle("/labels", mw.Default(CreateLabel)).Methods("POST")
-	Router.Handle("/labels/{idOrName}", mw.Default(GetLabel)).Methods("GET")
-	Router.Handle("/labels/{idOrName}", mw.Default(DeleteLabel)).Methods("DELETE")
-	Router.Handle("/labels/{idOrName}", mw.Default(UpdateLabel)).Methods("PUT")
-	Router.Handle("/labels/search", mw.Default(SearchLabels)).Methods("GET")
+
+	Router.Handle("/labels/{id}", mw.Default(GetLabel)).Methods("GET")
+	Router.Handle("/labels/{id}", mw.Default(DeleteLabel)).Methods("DELETE")
+	Router.Handle("/labels/{id}", mw.Default(UpdateLabel)).Methods("PUT")
 }
 
 // GetAllLabels will return a JSON array of all labels from the store.
@@ -25,28 +26,25 @@ func GetAllLabels(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(500)
 		w.Write([]byte(err.Error()))
+		log.Println(err)
 		return
 	}
 
-	jsn, err := json.Marshal(labels)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	w.Write(jsn)
+	sendJSON(w, labels)
 }
 
-// GetLabel will return a JSON representation of a model.
+// GetLabel will return a JSON representation of a label
 func GetLabel(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	id := r.URL.Path[len("/labels/"):]
 
 	lbl := &models.Label{}
 
-	i, err := strconv.Atoi(vars["idOrName"])
+	i, err := strconv.Atoi(id)
 	if err != nil {
-		lbl.Name = vars["idOrName"]
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
 	}
 
 	lbl.ID = int64(i)
@@ -54,75 +52,95 @@ func GetLabel(w http.ResponseWriter, r *http.Request) {
 	err = Store.Labels().Get(lbl)
 	if err != nil {
 		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
+		w.Write(apiError(err.Error()))
 		return
 	}
 
-	jsn, err := json.Marshal(lbl)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	w.Write(jsn)
+	sendJSON(w, lbl)
 }
 
 // CreateLabel creates a label in the db and return a JSON object of
 func CreateLabel(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+	var lbl models.Label
 
-	lbl := &models.Label{}
-
-	i, err := strconv.Atoi(vars["idOrName"])
-	if err != nil {
-		lbl.Name = vars["idOrName"]
-	}
-
-	lbl.ID = int64(i)
-
-	err = Store.Labels().New(lbl)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
+	u := mw.GetUser(r.Context())
+	if u == nil {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in to create a label"))
 		return
 	}
 
-	w.Write([]byte(lbl.String()))
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&lbl)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("malformed json"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Labels().New(&lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, lbl)
 }
 
 // UpdateLabel updates the label in the db and returns a message indicating
 // success or failure.
 func UpdateLabel(w http.ResponseWriter, r *http.Request) {
-	lbl := models.Label{}
+	var lbl models.Label
 
-	err := Store.Labels().Save(lbl)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
+	u := mw.GetUser(r.Context())
+	if u == nil {
+		w.WriteHeader(403)
+		w.Write(apiError("you must be logged in to create a label"))
 		return
 	}
 
-	w.Write([]byte("Label successfully updated"))
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&lbl)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("malformed json"))
+		log.Println(err)
+		return
+	}
 
+	err = Store.Labels().Save(lbl)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write(apiError(err.Error()))
+		log.Println(err)
+		return
+	}
+
+	sendJSON(w, lbl)
 }
 
 // DeleteLabel deletes labels from the db and returns a repsonse indicating
 // success of failure.
 func DeleteLabel(w http.ResponseWriter, r *http.Request) {
-	lbl := models.Label{}
+	vars := mux.Vars(r)
 
-	err := Store.Labels().Remove(lbl)
+	i, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write(apiError("invalid id"))
+		log.Println(err)
+		return
+	}
+
+	err = Store.Labels().Remove(models.Label{ID: int64(i)})
 	if err != nil {
 		w.WriteHeader(500)
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	w.Write([]byte("Label successfully deleted"))
-}
-
-// SearchLabels
-func SearchLabels(w http.ResponseWriter, r *http.Request) {
-
+	w.Write([]byte{})
 }
