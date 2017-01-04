a0c5ac52dfbeecb88d1e8eac151154c27c3845ba
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-04 12:15:26 -0500

    remove comment

--- store/pg/store.go
+++ store/pg/store.go
@@ -31,7 +31,6 @@ type Store struct {
 // New connects to the postgres database provided and returns a store
 // that's connected.
 func New(conn string, replicas ...string) store.Store {
-	// TODO: replica support
 
 	d, err := sql.Open("postgres", conn)
 	if err != nil {
