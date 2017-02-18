c4b3c530382ccb4dd3684dd034ea0f3257efbe44
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-17 22:37:43 -0500

    finish script for creating releases

--- release.sh
+++ release.sh
@@ -6,23 +6,10 @@
 # Requires go, npm, node, and curl be installed.
 #
 
-
 function parse_git_branch {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
     echo "${ref#refs/heads/} "
 }
-
-BRANCH=$(parse_git_branch)
-
-# if [ $BRANCH != "master" ] && [ $BRANCH != "develop" ]; then
-#     echo "you aren't on master or develop, refusing to package a release"
-#     exit 1
-# fi
-
-if [ "$GOOS" == "" ]; then
-    echo "\$GOOS not set defaulting to linux"
-    export GOOS="linux"
-fi
 
 function check_if_success() {
     if [ $? -ne 0 ]; then
@@ -33,23 +20,25 @@ function check_if_success() {
 
 function print_help() {
     echo "Usage: 
-    ./package.sh tag_name name_of_release prelease_bool:optional
+    ./release.sh tag_name name_of_release prelease_bool:optional
 
 Examples:
     This would deploy to tag v0.0.1 naming the release MVP and specify it is a 
     prerelease
 
-    ./package.sh v0.0.1 MVP true
+    ./release.sh v0.0.1 MVP true
 
-    This would deploy to tag v1.0.0 naming the release Aces High and specify it
-    as not a prerelease
+    If the 3rd argument is omitted we assume it is a normal release
 
-    ./package.sh v1.0.0 \"Aces High\" false
-
-    Alternatively prelease_bool can be omitted (defaults: false)
-
-    ./package.sh v1.0.0 \"Aces High\""
+    ./release.sh v1.0.0 \"Aces High\""
 }
+
+BRANCH=$(parse_git_branch)
+
+if [ $BRANCH != "master" ] && [ $BRANCH != "develop" ]; then
+    echo "you aren't on master or develop, refusing to package a release"
+    exit 1
+fi
 
 if [ "$1" == "--help" ] || [ "-h" == "$1" ]; then
     print_help
@@ -66,152 +55,172 @@ TAG_NAME=$1
 RELEASE_NAME=$2
 PRERELEASE=$3
 STARTING_DIR=$(pwd)
+PROGRAM="praelatus"
+ARCHES=("amd64")
+
+if [ -z "$OWNER" ]; then
+    OWNER=$USER
+fi
+
+if [ -z "$PLATFORMS" ]; then
+    PLATFORMS=("linux" "darwin" "windows")
+fi
 
 echo "Tag Name: $TAG_NAME"
 echo "Release Name: $RELEASE_NAME"
 echo "Prelease: $PRERELEASE"
-echo ""
+echo "Program: $PROGRAM"
+echo "Building for Arches: ${ARCHES[@]}"
+echo "Building for Platforms: ${PLATFORMS[@]}"
+echo "Repo Owner: $OWNER"
 
-# echo "Checking for dependencies..."
-# if ! [ -x "$(command -v go)" ]; then
-#     echo "You need to install the go tool. https://golang.org/download"
-#     exit 1
-# fi
+echo "Checking for dependencies..."
+if ! [ -x "$(command -v go)" ]; then
+    echo "You need to install the go tool. https://golang.org/download"
+    exit 1
+fi
 
-# if ! [ -x "$(command -v npm)" ]; then
-#     echo "You need to install npm. https://nodejs.org/en/download/"
-#     exit 1
-# fi
+if ! [ -x "$(command -v npm)" ]; then
+    echo "You need to install npm. https://nodejs.org/en/download/"
+    exit 1
+fi
 
-# if ! [ -x "$(command -v node)" ]; then
-#     echo "You need to install node. https://nodejs.org/en/download/"
-#     exit 1
-# fi
+if ! [ -x "$(command -v node)" ]; then
+    echo "You need to install node. https://nodejs.org/en/download/"
+    exit 1
+fi
 
-# if ! [ -x "$(command -v curl)" ]; then
-#     echo "You need to install curl"
-#     exit 1
-# fi
+if ! [ -x "$(command -v curl)" ]; then
+    echo "You need to install curl"
+    exit 1
+fi
 
-# if ! [ -x "$(command -v yarn)" ]; then
-#     echo "yarn not detected attempting to install..."
-#     sudo npm install -g yarn
-# fi
+if ! [ -x "$(command -v yarn)" ]; then
+    echo "yarn not detected attempting to install..."
+    sudo npm install -g yarn
+fi
 
-# if ! [ -x "$(command -v webpack)" ]; then
-#     echo "webpack not detected attempting to install..."
-#     sudo npm install -g webpack
-# fi
+if ! [ -x "$(command -v webpack)" ]; then
+    echo "webpack not detected attempting to install..."
+    sudo npm install -g webpack
+fi
 
-# if ! [ -x "$(command -v glide)" ]; then
-#     echo "glide not detected attempting to install..."
-#     go get github.com/Masterminds/glide
-#     if ! [ -x "$(command -v glide)" ]; then
-#         echo "installed glide but \$GOBIN isn't in \$PATH"
-#         exit 1
-#     fi
-# fi
+if ! [ -x "$(command -v glide)" ]; then
+    echo "glide not detected attempting to install..."
+    go get github.com/Masterminds/glide
+    if ! [ -x "$(command -v glide)" ]; then
+        echo "installed glide but \$GOBIN isn't in \$PATH"
+        exit 1
+    fi
+fi
 
-# if [ -d "build" ]; then
-#     echo "cleaning build directory..."
-#     rm -rf build
-# fi
+if [ -d "build" ]; then
+    echo "cleaning build directory..."
+    rm -rf build
+fi
 
-# # create the final build directories
-# mkdir build/
-# mkdir build/{client,linux,windows,darwin}
+# create the final build directories
+mkdir build/
+mkdir build/client
 
-# # # get the frontend
-# # echo "downloading the frontend"
-# # git clone https://github.com/praelatus/frontend build/frontend
+# get the frontend
+echo "downloading the frontend"
+git clone https://github.com/praelatus/frontend $STARTING_DIR/build/frontend
 
-# # # change to frontend git repo
-# # cd build/frontend
+# change to frontend git repo
+cd $STARTING_DIR/build/frontend
 
-# # # install frontend deps
-# # echo "installing dependencies for frontend"
-# # yarn install
+echo "installing dependencies for frontend"
+yarn install
 
-# # echo "compiling the frontend"
-# # webpack -p
-# # mv $STARTING_DIR/build/frontend/build/debug/static $STARTING_DIR/build/client/
-# # cp $STARTING_DIR/build/frontend/index.html $STARTING_DIR/build/client/index.html
+echo "compiling the frontend"
+webpack -p
+mv $STARTING_DIR/build/frontend/build/debug/static $STARTING_DIR/build/client/
+cp $STARTING_DIR/build/frontend/index.html $STARTING_DIR/build/client/index.html
 
-# # install deps for backend
-# echo "installing dependencies for backend"
-# glide install
+echo "installing dependencies for backend"
+glide install
 
-# echo "cleaning up"
-# cd $STARTING_DIR
-# rm -rf build/frontend
+echo "cleaning up"
+cd $STARTING_DIR
+rm -rf build/frontend
 
-# GOARCH="amd64"
+PACKAGES=()
 
-# platforms=("linux" "darwin" "windows")
+for platform in "${PLATFORMS[@]}"
+do
+    for arch in "${ARCHES[@]}"
+    do
+        echo "compiling the backend for $platform-$arch"
+        mkdir build/$platform-$arch
 
-# for platform in "${platforms[@]}"
-# do
-#     GOOS=$platform
-#     # compile the backend
-#     echo "compiling the backend for $GOOS"
-#     if [ "$GOOS" == "windows" ]; then
-#         go build -o build/$GOOS/praelatus.exe >/dev/null
-#     else
-#         go build -o build/$GOOS/praelatus >/dev/null
-#     fi
-#     check_if_success
+        GOOS=$platform
+        GOARCH=$arch 
+        if [ "$platform" == "windows" ]; then
+            go build -o build/$platform-$arch/$PROGRAM.exe >/dev/null
+        else
+            go build -o build/$platform-$arch/$PROGRAM >/dev/null
+        fi
 
-#     cp -R build/client build/$GOOS/client
+        # make sure builds worked
+        check_if_success
 
-#     echo "building release tar"
-#     cd build/$GOOS
-    
-#     PACKAGE_NAME="praelatus-$TAG_NAME-$GOOS-$GOARCH.tar.gz"
-    
-#     if ! [ -z "$PRERELEASE" ]; then
-#         PACKAGE_NAME="praelatus-$TAG_NAME-prelease-$GOOS-$GOARCH.tar.gz"
-#     fi
-    
-#     echo $PACKAGE_NAME
-#     if [ -f "$STARTING_DIR/$PACKAGE_NAME" ]; then
-#         echo "old package detected removing..."
-#         rm $STARTING_DIR/$PACKAGE_NAME
-#     fi
-    
-#     tar czf $STARTING_DIR/$PACKAGE_NAME *
+        echo "building release tar"
+        cd build/$platform-$arch
 
-#     cd $STARTING_DIR
-# done
+        PACKAGE_NAME="$PROGRAM-$TAG_NAME-$platform-$arch.tar.gz"
+        if [ "$platform" == "windows" ]; then
+            PACKAGE_NAME="$PROGRAM-$TAG_NAME-$platform-$arch.zip"
+        fi
 
-# # create the tag
-# echo "tagging release..."
-# if [ $TAG_NAME == "nightly" ]; then
-#     # we just move nightly up to the current commit instead of retagging
-#     git tag -af $TAG_NAME -m $RELEASE_NAME
-# else
-#     git tag -a $TAG_NAME -m $RELEASE_NAME
-# fi
+        echo $PACKAGE_NAME
+        PACKAGES+=("$PACKAGE_NAME")
 
-# # push the tag
-# echo "Pushing tags..."
-# git push --follow-tags
+        if [ -f "$STARTING_DIR/$PACKAGE_NAME" ]; then
+            echo "old package detected removing..."
+            rm $STARTING_DIR/$PACKAGE_NAME
+        fi
 
-# if [ -z "$GITHUB_API_TOKEN" ]; then
-#     echo "no github token detected all done."
-#     exit 0
-# fi
+        if [ "$platform" == "windows" ]; then
+            zip $STARTING_DIR/$PACKAGE_NAME *
+        else
+            tar czf $STARTING_DIR/$PACKAGE_NAME *
+        fi
 
-GITHUB_URL="https://api.github.com/repos/$REPO_OWNER/$REPOSITORY/releases?access_token=$GITHUB_API_TOKEN"
-JSON="{ 
-    \"tag_name\": \"$TAG_NAME\", 
-    \"name\": \"$RELEASE_NAME\" 
-    \"body\": \"Praelatus release \\\"$RELEASE_NAME\\\"\"
-    \"draft\": false,
-    \"target_commitsh\": \"master\"
-}"
+        cd $STARTING_DIR
+    done
+done
 
-curl -X POST --data $JSON $GITHUB_URL
+# create the tag
+echo "tagging release..."
+git tag -a $TAG_NAME -m $RELEASE_NAME
 
-echo $JSON
+# push the tag
+echo "Pushing tags..."
+git push --follow-tags
 
+if [ -z "$GITHUB_API_TOKEN" ]; then
+    echo "no github token detected all done."
+    exit 0
+fi
 
+GITHUB_URL="https://api.github.com/repos/$OWNER/$PROGRAM/releases?access_token=$GITHUB_API_TOKEN"
+JSON="{ \"tag_name\": \"$TAG_NAME\", \"name\": \"$RELEASE_NAME\", \"body\": \"$PROGRAM release $RELEASE_NAME\", \"target_commitsh\": \"master\" }"
+
+RESP=$(curl -X POST --data "$JSON" $GITHUB_URL)
+ASSETS_URL=$(echo "$RESP" | grep -oP '(?<="upload_url": ")(.*assets)')
+
+for pkg in "${PACKAGES[@]}"
+do
+    echo "uploading $pkg"
+    UPLOAD_URL="$ASSETS_URL?name=$pkg&access_token=$GITHUB_API_TOKEN"
+    echo $UPLOAD_URL
+
+    if [ -z "$(echo $pkg | grep -o ".zip")" ]; then
+        HEADERS="Content-Type:application/zip"
+    else
+        HEADERS="Content-Type:application/gzip"
+    fi
+
+    curl -X POST -H $HEADERS --data-binary $STARTING_DIR/$pkg $UPLOAD_URL >/dev/null
+done
