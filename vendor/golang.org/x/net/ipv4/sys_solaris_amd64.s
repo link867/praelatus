a831964378388d6cab62034b331821f841fe3022
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-02-20 15:47:22 -0500

    add gorilla websockets'

--- vendor/golang.org/x/net/ipv4/sys_solaris_amd64.s
+++ vendor/golang.org/x/net/ipv4/sys_solaris_amd64.s
@@ -1,10 +1,8 @@
-// Copyright 2014 The Go Authors.  All rights reserved.
+// Copyright 2016 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !gccgo
-
 #include "textflag.h"
 
-TEXT ·use(SB),NOSPLIT,$0
-	RET
+TEXT ·sysvicall6(SB),NOSPLIT,$0-88
+	JMP	syscall·sysvicall6(SB)
